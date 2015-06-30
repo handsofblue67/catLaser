4d8b3cd04d34c0a55701bef497158e3976ba61ce
Michael Robison <michael.robison168@gmail.com>
2015-06-29 21:55:16 -0600

    fixed the y axis bug.

--- catLaser.ino
+++ catLaser.ino
@@ -17,29 +17,25 @@ void setup()
   servoY.attach(8);
   servoX.attach(9);
   //Serial.begin(9600);
+  
   //noise from analog pin 0 will create a 'random' seed for RNG
   randomSeed(analogRead(0));
 }
 
 void loop() 
 {
-	rate = random(30, 60);
-        
-        randY = random(25, 75);
-	randX = random(45, 135);
-        //Serial.print("randX and y: ");
-        //Serial.println(randX);
-        //Serial.println(randY);
-        //Serial.print("Starting RNG\n");
- 	
-        posX = servoX.read();
-	posY = servoY.read();
-	while(posX != randX || posY != randY)
+        randomize();
+		delay(2000);
+}
+
+void move()
+{
+        while(posX != randX || posY != randY)
 	{    
-                //Serial.print("posX and y: ");
-                //Serial.println(posX);
-                //Serial.println(posY);
-                //Serial.print("\n");
+/*              Serial.print("posX and y: ");
+                Serial.println(posX);
+                Serial.println(posY);
+                Serial.print("\n");*/
 		
                 if(posX < randX)
 		{
@@ -51,38 +47,57 @@ void loop()
 				delay(rate+5);
 		}
     
-		else if(posX > randX)
-		{
-			servoX.write(--posX);
-			if(posY < 50)
-				delay(rate-5);
-      
-			else
-				delay(rate+5);
-		}
-                 
-                delay(1);
-                
+				else if (posX > randX)
+				{
+					servoX.write(--posX);
+					if (posY < 50)
+						delay(rate - 5);
+
+					else
+						delay(rate + 5);
+
+
+					delay(1);
+				}
 		if(posY < randY)
 		{
-			servoX.write(++posY);
-			if(posY < 50)
-			  delay(rate-5);
-        
-			else
-                          delay(rate+5);
-		}
-    
-		else if(posY > randY)
-		{
-			servoY.write(--posY);
+			servoY.write(posY++);
 			if(posY < 50)
 				delay(rate-5);
-      
+        
 			else
 				delay(rate+5);
 		}
-                
-                delay(1);
+    
+		else if (posY > randY)
+		{
+			servoY.write(posY--);
+			if (posY < 50)
+				delay(rate - 5);
+
+			else
+				delay(rate + 5);
+
+
+			delay(1);
+		}
+		
 	}
+}
+
+void randomize()
+{
+      	rate = random(20, 60);
+        
+        randY = random(25, 75);
+		randX = random(45, 135);
+        /*Serial.print("randX and y: ");
+        Serial.println(randX);
+        Serial.println(randY);
+        Serial.print("Starting RNG\n");*/
+ 	
+        posY = servoY.read();	
+        posX = servoX.read();
+        
+        move();
 }
