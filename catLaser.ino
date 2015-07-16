ff4293bdbdf8e151e7a2c1e1fb61da3945da87d1
Michael Robison <michael.robison168@gmail.com>
2015-07-15 18:41:32 -0600

    implemented new library
    want to edit library to change its speed as it goes, rather than a
    constant speed throughout the roation

--- catLaser.ino
+++ catLaser.ino
@@ -1,70 +1,50 @@
 #include <Arduino.h>
-#include <Servo.h>
+#include <VarSpeedServo.h>
 
+VarSpeedServo yServo;  // create servo object to control a servo
+VarSpeedServo xServo;
+
+const int yServoPin = 8; // the digital pin used for the first servo
+const int xServoPin = 9; // the digital pin used for the second servo
 const int rate = 20;
 
-Servo servoY;
-Servo servoX;
+int xRand;
+int yRand;
 
-int randX;
-int randY;
-  
-int posX;
-int posY;
+int xPos;
+int yPos;
 
-void setup() 
-{	
-  // initialize the servos
-  servoY.attach(8);
-  servoX.attach(9);
-  
-  //noise from analog pin 0 will create a 'random' seed for RNG
-  randomSeed(analogRead(0));
+void setup() {
+	yServo.attach(yServoPin);  // attaches the servo on pin 8 to the servo object
+	yServo.write(0, 255, false); // set the intial position of the servo, as fast as possible, run in background
+	xServo.attach(xServoPin);  // attaches the servo on pin 9 to the servo object
+	xServo.write(0, 255, true);  // set the intial position of the servo, as fast as possible, wait until done
+
+	//noise from analog pin 0 will create a 'random' seed for RNG
+	randomSeed(analogRead(0));
 }
 
-void loop() 
+void loop()
 {
-        randomize();
+	randomize();
 	delay(2000);
 }
 
 void move()
 {
-        while(posX != randX || posY != randY)
-	{    
-		if(posX < randX)
-		{
-		    servoX.write(++posX);      
-		    delay(posY-rate);
-		}
-    
-		else if (posX > randX)
-		{
-			servoX.write(--posX);
-			delay(posY-rate);
-		}
-
-		if(posY < randY)
-		{
-			servoY.write(posY++);
-			delay(posY-rate);
-		}
-    
-		else if (posY > randY)
-		{
-			servoY.write(posY--);
-			delay(posY-rate);
-		}
-	}
+	yServo.write(yRand, 48 * tan(yPos * 3.14 / 180) - 20, false);
+	xServo.write(xRand, 48 * tan(yPos * 3.14 / 180) - 20, true);
 }
 
 void randomize()
 {
-        randY = random(35, 75);
-	randX = random(45, 135);
- 	
-        posY = servoY.read();	
-        posX = servoX.read();
-        
-        move();
+	yRand = random(20, 72);
+	xRand = random(45, 135);
+
+	yPos = yServo.read();
+	xPos = xServo.read();
+
+	move();
 }
+
+
